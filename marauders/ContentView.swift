25673bbdd31765036cb467cdfb45b1bbe9d48aec
somsakGitHub <somsak.kaeworasan@outlook.com>
2024-04-13 11:30:33 +0700

    first commit.

---
+++ marauders/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  marauders
+//
+//  Created by somsak on 13/4/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ marauders/maraudersApp.swift
@@ -0,0 +1,17 @@
+//
+//  maraudersApp.swift
+//  marauders
+//
+//  Created by somsak on 13/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct maraudersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
