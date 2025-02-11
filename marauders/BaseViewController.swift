fcf393dde30f3c86c7a68de83d2c3dae7de967cf
somsak <somsakk@tisco.co.th>
2025-02-11 10:42:21 +0700

    Reface directory.

---
+++ marauders/BaseViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class BaseViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+//    func setWindowRootView(scene: Scene) {
+//        window?.rootViewController = sceneFactory.makeScene(scene: scene)
+//    }
+}
